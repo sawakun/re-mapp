acd5f14b425ff6f3e9a83c170bc2cb8f59c47508
nishiba <nishiba.m@gmail.com>
2013-08-11 21:01:23 +0900

    fix some conflicts.

--- ReMapp1/RMPRearrangedView.m
+++ ReMapp1/RMPRearrangedView.m
@@ -9,30 +9,18 @@
 #import "RMPRearrangedView.h"
 
 @implementation RMPRearrangedView
-<<<<<<< HEAD
-static CGFloat originY;
-=======
 CGFloat originY;
->>>>>>> origin/new/TimeLine
 
 - (void)setOriginY:(CGFloat)aOriginY
 {
     originY = aOriginY;
 }
-<<<<<<< HEAD
-
-=======
 /*
->>>>>>> origin/new/TimeLine
 - (void)layoutSubviews
 {
     [super layoutSubviews];
     self.layer.position = CGPointMake(self.frame.origin.x + 0.5 * self.frame.size.width,
                                       originY + 0.5 * self.frame.size.height);
 }
-<<<<<<< HEAD
-
-=======
 */
->>>>>>> origin/new/TimeLine
 @end
