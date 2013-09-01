5289532748f1f956155384fca78a069a581de085
Yohei Sawayama <yohei.sawayama@gmail.com>
2013-09-01 22:54:03 +0900

    implement sendNewBuzzWithUserSystemId (post Json to server)

--- ReMapp1/RMPHTTPConnection.m
+++ ReMapp1/RMPHTTPConnection.m
@@ -15,7 +15,7 @@
                            Location:(CLLocationCoordinate2D)location
                               Image:(UIImage*)image
 {    
-    // JSON to post
+    // post JSON like as follows
     /*
      {
      "user_id":3,
@@ -26,93 +26,116 @@
      "buzz_type":"eat"
      }
      */
-    //uploading image
-
-    NSString *url=@"";
-    NSMutableURLRequest *requestForImage;
-    requestForImage =
-    [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://re-mapp.herokuapp.com/api/uploadTest"]
-                            cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0];
     
-    NSData *imageData = [[NSData alloc] initWithData:UIImagePNGRepresentation(image)];
-    //set HTTP POST
-    [requestForImage setHTTPMethod:@"POST"];
-    [requestForImage setValue:@"image/png" forHTTPHeaderField:@"Accept"];
-    [requestForImage setValue:@"image/png" forHTTPHeaderField:@"Content-Type"];
-    [requestForImage setValue:[NSString stringWithFormat:@"%d",
-                       [imageData length]] forHTTPHeaderField:@"Content-Length"];
-    [requestForImage setHTTPBody: imageData];
+    //upload image and get url
+    NSString *buzz_img_url=[self sendImage:image userSystemId:userSystemId];
     
-    //response
-    NSURLResponse *respForImage=nil;
-    NSError *errForImage=nil;
-    
-    NSData *resultForImage;
-    NSArray *resultArrayForImage;
-    BOOL *flagForImage = TRUE;
-    while(flagForImage){
-        //HTTP request send
-        resultForImage = [NSURLConnection sendSynchronousRequest:requestForImage
-                                       returningResponse:&respForImage error:&errForImage];
-        resultArrayForImage = [NSJSONSerialization JSONObjectWithData:resultForImage
-                                                      options:NSJSONReadingAllowFragments
-                                                                error:&errForImage];
-    }
-    if([resultArrayForImage count]!=0) url=resultArrayForImage[0];
-    
-    //TODO: treating buzz_type
     // upload buzz data
     NSMutableDictionary *mutableDic = [NSMutableDictionary dictionary];
-    [mutableDic setValue:[NSString stringWithFormat:@"%d", userSystemId] forKey:@"user_id"];
+    [mutableDic setValue:[NSNumber numberWithInt:userSystemId] forKey:@"user_id"];
     [mutableDic setValue:buzzText forKey:@"buzz_body"];
-    [mutableDic setValue:url forKey:@"buzz_img_url"];
-    [mutableDic setValue:[NSString stringWithFormat:@"%f", location.latitude] forKey:@"lat"];
-    [mutableDic setValue:[NSString stringWithFormat:@"%f", location.longitude] forKey:@"lon"];
+    [mutableDic setValue:buzz_img_url forKey:@"buzz_img_url"];
+    [mutableDic setValue:[NSNumber numberWithDouble:location.latitude] forKey:@"lat"];
+    [mutableDic setValue:[NSNumber numberWithDouble:location.longitude] forKey:@"lon"];
+    //TODO: treating buzz_type
     [mutableDic setValue:@"buzz" forKey:@"buzz_type"];
     
     //convert to JSON
-    NSError*   error = nil;
-    NSData *requestData = [NSJSONSerialization dataWithJSONObject:mutableDic options:NSJSONWritingPrettyPrinted error:&error];
+    NSError*   errorJson = nil;
+    NSData *requestData = [NSJSONSerialization dataWithJSONObject:mutableDic options:NSJSONWritingPrettyPrinted error:&errorJson];
+
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://re-mapp.herokuapp.com/api/post"] cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0];
+
+// for debug. send to localhost
+//    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://localhost:9000/api/post"] cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0];
     
-    //debug
-    NSArray *array = [NSJSONSerialization JSONObjectWithData:requestData
-                                                     options:NSJSONReadingAllowFragments
-                                                       error:&error];
-    
-    NSMutableURLRequest *request;
-    request =
-    [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://re-mapp.herokuapp.com/api/post"]
-                            cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0];
     
     //set HTTP POST
     [request setHTTPMethod:@"POST"];
     [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
-    [request setValue:@"application/json"
-   forHTTPHeaderField:@"Content-Type"];
-    [request setValue:[NSString stringWithFormat:@"%d",
-                       [requestData length]] forHTTPHeaderField:@"Content-Length"];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    [request setValue:[NSString stringWithFormat:@"%d", [requestData length]] forHTTPHeaderField:@"Content-Length"];
     [request setHTTPBody: requestData];
     
     //response
-    NSURLResponse *resp=nil;
-    NSError *err=nil;
-    
+    NSURLResponse *response=nil;
+    NSError *error=nil;
     NSData *result;
-    NSArray *resultArray;
-    BOOL *flag = TRUE;
+    NSMutableDictionary *resultDic;
+    BOOL flag = YES;
     while(flag){
         //HTTP request send
         result = [NSURLConnection sendSynchronousRequest:request
-                                           returningResponse:&resp error:&err];
-        resultArray = [NSJSONSerialization JSONObjectWithData:result
+                                           returningResponse:&response error:&error];
+        resultDic = [NSJSONSerialization JSONObjectWithData:result
                                                      options:NSJSONReadingAllowFragments
-                                                       error:&error];
-        //TODO: check succeeded in sending
-        //TODO: show dialog and ask 'resend' or 'save to draft'
-        flag=FALSE;
+                                                       error:&errorJson];
+        NSString *status = [resultDic objectForKey:@"status"];
+        if(![status isEqualToString:@"ok"]){
+            //TODO: show dialog and ask 'resend' or 'save to draft'
+            
+        }else{
+            // successfully posted
+            flag=NO;
+        }
     }
     return TRUE;
 }
+
+
++ (NSString*)sendImage:(UIImage*)image userSystemId:(NSInteger)userSystemId
+{
+    //upload image
+    NSString *url=@"";
+    
+/*
+    NSMutableURLRequest *request =[NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://re-mapp.herokuapp.com/api/uploadTest"]
+                            cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0];
+    
+    NSData *imageData = [[NSData alloc] initWithData:UIImagePNGRepresentation(image)];
+
+    //set HTTP POST
+    //TODO: treat image format png/jpg/gif
+    [request setHTTPMethod:@"POST"];
+    [request setValue:@"image/png" forHTTPHeaderField:@"Accept"];
+    [request setValue:@"image/png" forHTTPHeaderField:@"Content-Type"];
+    [request setValue:[NSString stringWithFormat:@"%d", [imageData length]] forHTTPHeaderField:@"Content-Length"];
+    [request setHTTPBody: imageData];
+    
+    //response
+    NSURLResponse *response=nil;
+    NSError *error=nil;
+    NSError *errorJson=nil;
+    NSData *result;
+    NSMutableDictionary *resultDic;
+    BOOL flag = YES;
+    while(flag){
+        //HTTP request send
+        result = [NSURLConnection sendSynchronousRequest:request
+                                       returningResponse:&response error:&error];
+        resultDic = [NSJSONSerialization JSONObjectWithData:result
+                                                    options:NSJSONReadingAllowFragments
+                                                      error:&errorJson];
+        NSString *status = [resultDic objectForKey:@"status"];
+        if(![status isEqualToString:@"ok"]){
+            //TODO: show dialog and ask 'resend' or 'save to draft'
+            
+        }else{
+            // successfully posted
+            flag=NO;
+            url=[resultDic objectForKey:@"url"];
+        }
+    }
+*/
+    
+    // for dev
+    url=@"http://re-mapp.herokuapp.com/assets/images/IMG_0732.jpg";
+    return url;
+}
+
+
+
+
 
 + (BOOL)sendModifiedUserName:(NSString*)name
                        Email:(NSString*)email
